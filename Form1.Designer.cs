47a2c39d2ed840afdf5c08cde405731fdb9c1a92
Chris <britolopez.christopher.2023@gmail.com>
2023-11-10 18:27:16 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace U3_Practica_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(12, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(358, 27);
+            label1.TabIndex = 0;
+            label1.Text = "Sumar los números capturados";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(18, 98);
+            label2.Name = "label2";
+            label2.Size = new Size(174, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Introduce un número";
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = Color.MediumSlateBlue;
+            listBox1.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 21;
+            listBox1.Location = new Point(205, 98);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(114, 235);
+            listBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.MediumSlateBlue;
+            textBox1.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(44, 139);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 28);
+            textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(18, 365);
+            label3.Name = "label3";
+            label3.Size = new Size(181, 21);
+            label3.TabIndex = 4;
+            label3.Text = "La suma de todos es:";
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.MediumSlateBlue;
+            textBox2.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(205, 362);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 28);
+            textBox2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.CornflowerBlue;
+            button1.Location = new Point(59, 187);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 6;
+            button1.Text = "Añadir";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.CornflowerBlue;
+            button2.Location = new Point(59, 304);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 7;
+            button2.Text = "Sumar";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.SlateBlue;
+            ClientSize = new Size(377, 414);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private Label label3;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+    }
+}
